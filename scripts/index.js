3ba7d82f28184b944dd3f8884fd70b96367123fc
modo_lv <modo-lv@users.noreply.github.com>
2025-12-27 15:28:06 +0200

    Move bank account behind URL param.

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,43 @@
-let element = document.getElementById("swedbank")
+Object.prototype.also = function (f) {
+    f(this)
+    return this
+}
 
-let popup = tippy(element, {
-    content: "IBAN → 📋️",
-    trigger: "manual",
-    onShow(instance) {
-        setTimeout(() => {
-            instance.hide();
-        }, 1500);
-    }
+window.addEventListener("DOMContentLoaded", () => {
+    if (!new URLSearchParams(window.location.search).has("money"))
+        return
+
+    let swedbank =
+        document.createElement("a").also((a) => {
+            a.setAttribute("href", "#")
+            a.innerText = "LV80HABA0551005403902"
+        })
+
+    document
+        .querySelector("h1")
+        .insertAdjacentElement("afterend", document.createElement("section").also(section =>
+            section.appendChild(swedbank))
+        )
+        .insertAdjacentElement("afterend", document.createElement("hr"))
+
+    let popup = tippy(swedbank, {
+        content: "IBAN → 📋️",
+        trigger: "manual",
+        onShow(instance) {
+            setTimeout(() => {
+                instance.hide();
+            }, 1500);
+        }
+    });
+    swedbank.addEventListener("click", async (event) => {
+        let iban = event.currentTarget.text
+        try {
+            await navigator.clipboard.writeText(iban)
+            popup.setContent(`${iban} → 📋️️`)
+            popup.show()
+        } catch (error) {
+            console.error(error.message)
+        }
+    })
+
 });
-
-element.addEventListener("click", async (e) => {
-    let iban = e.currentTarget.text
-    try {
-        await navigator.clipboard.writeText(iban)
-        popup.setContent(`${iban} → 📋️️`)
-        popup.show()
-    } catch (error) {
-        console.error(error.message)
-    }
-})
